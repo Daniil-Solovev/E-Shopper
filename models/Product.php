80ad0c2a46af3079122e8e78af7155815096daef
Daniil-Solovev <frontend.servise@gmail.com>
2018-01-26 20:35:18 +0300

    Инициализация проекта. Реализация MVC, подключение к БД, создание шаблонов

---
+++ models/Product.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Desktop
+ * Date: 26.01.2018
+ * Time: 14:24
+ */
+
+class Product
+{
+
+}
---
+++ views/news/archive.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Desktop
+ * Date: 26.01.2018
+ * Time: 15:29
+ */
